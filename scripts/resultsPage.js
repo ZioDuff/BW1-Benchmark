5e46161c087f52d7722e187bfefb4c773c7ed991
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-03-21 17:28:52 +0100

    Css result page

--- scripts/resultsPage.js
+++ scripts/resultsPage.js
@@ -1,47 +1,40 @@
-const risposteGiuste = localStorage.getItem("correctQuestion");
-const giuste = risposteGiuste;
-
-console.log(giuste);
-
 const btn = document.getElementById("change");
 btn.addEventListener("click", () => {
   window.location.href = "rateUs.html";
 });
-  window.location.href = "rateUs.html"
-})
 
-const risposteDate = localStorage.getItem("allAnswer")
-console.log(risposteDate)
+const risposteDate = localStorage.getItem("allAnswer");
+console.log(risposteDate);
 
-const arrayRisposte = risposteDate.split(",")
-console.log(arrayRisposte)
+const arrayRisposte = risposteDate.split(",");
+console.log(arrayRisposte);
 
-let indicePositivo = 0
-let indiceNegativo = 0
+let indicePositivo = 0;
+let indiceNegativo = 0;
 
-const giuste = []
+const giuste = [];
 
 arrayRisposte.forEach((element) => {
   if (element === "true") {
-    indicePositivo++
+    indicePositivo++;
   } else {
-    indiceNegativo++
+    indiceNegativo++;
   }
-})
-console.log(indicePositivo)
-console.log(indiceNegativo)
+});
+console.log(indicePositivo);
+console.log(indiceNegativo);
 
-const paragraph = document.querySelector("p")
+const paragraph = document.querySelector("p");
 if (indicePositivo > indiceNegativo) {
-  paragraph.innerText = "congratulazioni"
+  paragraph.innerText = "congratulazioni";
 } else {
-  paragraph.innerText = "sei na pippa"
+  paragraph.innerText = "sei na pippa";
 }
 
-indicePositivo = (indicePositivo / arrayRisposte.length) * 100
-console.log(indicePositivo + "%")
+indicePositivo = (indicePositivo / arrayRisposte.length) * 100;
+console.log(indicePositivo + "%");
 
-const correctDiv = document.querySelector(".center")
-const correctH1 = document.createElement("h1")
-correctH1.innerText = "correct" + " " + indicePositivo + "%"
-correctDiv.appendChild(correctH1)
+const correctDiv = document.querySelector(".center");
+const correctH1 = document.createElement("h1");
+correctH1.innerText = "correct" + " " + indicePositivo + "%";
+correctDiv.appendChild(correctH1);
