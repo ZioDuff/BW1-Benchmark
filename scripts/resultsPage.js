f8134227bf4bc1cab6ad7bcd95bc005f52e72d83
Jacopo De maio <jajodemaio@gmail.com>
2024-03-21 20:21:55 +0100

    js results page change

--- scripts/resultsPage.js
+++ scripts/resultsPage.js
@@ -1,40 +1,63 @@
-const btn = document.getElementById("change");
+const btn = document.getElementById("change")
 btn.addEventListener("click", () => {
-  window.location.href = "rateUs.html";
-});
+  window.location.href = "rateUs.html"
+})
 
-const risposteDate = localStorage.getItem("allAnswer");
-console.log(risposteDate);
+const risposteDate = localStorage.getItem("allAnswer")
+console.log(risposteDate)
 
-const arrayRisposte = risposteDate.split(",");
-console.log(arrayRisposte);
+const arrayRisposte = risposteDate.split(",")
+console.log(arrayRisposte)
 
-let indicePositivo = 0;
-let indiceNegativo = 0;
+let indicePositivo = 0
+let indiceNegativo = 0
 
-const giuste = [];
+const giuste = []
 
 arrayRisposte.forEach((element) => {
   if (element === "true") {
-    indicePositivo++;
+    indicePositivo++
   } else {
-    indiceNegativo++;
+    element === false
+    indiceNegativo++
   }
-});
-console.log(indicePositivo);
-console.log(indiceNegativo);
+})
+console.log(indicePositivo)
+console.log(indiceNegativo)
 
-const paragraph = document.querySelector("p");
+//stampiamo la parte centrale del grafico
+
+//stampa in mezzo al cerchio
+const middelDiv = document.querySelector("#result")
+const paragraph = document.createElement("p")
+middelDiv.appendChild(paragraph)
 if (indicePositivo > indiceNegativo) {
-  paragraph.innerText = "congratulazioni";
+  paragraph.innerText = "congratulazioni"
 } else {
-  paragraph.innerText = "sei na pippa";
+  paragraph.innerText = "sei na pippa"
 }
 
-indicePositivo = (indicePositivo / arrayRisposte.length) * 100;
-console.log(indicePositivo + "%");
+// stampiamo sulla destra il riusltat correct
+let correctPercentage = (indicePositivo / arrayRisposte.length) * 100
+const correctDiv = document.querySelector(".correct")
+const correctH1 = document.createElement("h1")
+correctH1.innerText = "correct" + " " + correctPercentage + "%"
+let correctNumAnswer =
+  indicePositivo + "/" + arrayRisposte.length + " " + "questions"
+const correctH2 = document.createElement("h2")
+correctH2.innerText = correctNumAnswer
+correctDiv.appendChild(correctH1)
+correctDiv.appendChild(correctH2)
 
-const correctDiv = document.querySelector(".center");
-const correctH1 = document.createElement("h1");
-correctH1.innerText = "correct" + " " + indicePositivo + "%";
-correctDiv.appendChild(correctH1);
+//stampaimo sulla sinistra il risultato wrong
+
+let wrongPercentage = (indiceNegativo / arrayRisposte.length) * 100
+const wrongDiv = document.querySelector(".wrong")
+const wrongH1 = document.createElement("h1")
+wrongH1.innerText = "wrong" + " " + wrongPercentage + "%"
+let wrongNumAnswer =
+  indiceNegativo + "/" + arrayRisposte.length + " " + "questions"
+const wrongH2 = document.createElement("h2")
+wrongH2.innerText = wrongNumAnswer
+wrongDiv.appendChild(wrongH1)
+wrongDiv.appendChild(wrongH2)
